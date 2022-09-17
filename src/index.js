b185626bd054f84f99f00b25dcfc6f56bad78239
juan.sosa1 <juan.sosa@squad.com.ar>
2022-09-17 00:37:40 -0300

    remove comments

--- src/index.js
+++ src/index.js
@@ -9,23 +9,3 @@ async function main(){
 }
 
 main();
-
-// const qrcode = require('qrcode-terminal');
-
-// const {Client} = require('whatsapp-web.js');
-
-// const client2 = new Client();
-
-// client2.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
-
-// client2.on('ready', () => {
-//     console.log('Cliente 2 logueado!!!');
-// });
-
-// client2.on('message', msg => {
-//     client2.sendMessage(msg.from, 'Hola! Soy un bot que devuelve el mismo msj que recibió :D\nTu mensaje fue: ' + msg.body);
-// });
-
-// client2.initialize();
